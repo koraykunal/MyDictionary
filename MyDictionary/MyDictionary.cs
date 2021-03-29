fdd460458c31027855db24705b5d2ae6fa41f9d4
Koray Kunal <koraykunal85@gmail.com>
2021-03-29 23:14:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key,Value>
+    {
+        (Key,Value)[] items;
+        public MyDictionary()
+        {
+            items = new (Key, Value)[0];
+        }
+        public void Add(Key key, Value value)
+        {
+            (Key,Value)[] tempArray = items;
+            items = new (Key,Value)[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = (key, value);
+
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public void Listing()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> programmingLangs = new MyDictionary<int, string> { };
+            programmingLangs.Add(1, "Python");
+            programmingLangs.Add(2, "C#");
+            programmingLangs.Add(3, "Java");
+            programmingLangs.Add(4, "JavaScript");
+            programmingLangs.Add(5, "Lua");
+            programmingLangs.Listing();
+        }
+    }
+}
